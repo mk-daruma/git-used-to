9deafae75b8689f7e0dc86d29978ada4960157ba
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-08 22:11:31 +0900

    [update]セキュリティ対策・セッションを無効にする記述を追加 #3
    CSRF 対策のために下記の記述を追加。(懸念事項:CSRFについての理解が浅く、記述内容を修正する必要が出てくる可能性あり。)
    protect_from_forgery with: :execption
    また、今回はdevise_token_authを使用しているためセッションを無効にするための記述も行った。

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :execption
+  before_action :skip_session
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+
+  protected
+    def
+      request.session_options[:skip] = true
+    end
 end
