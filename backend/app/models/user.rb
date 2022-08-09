634901d43417e339f418426d355cc49679aa1deb
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-09 17:10:53 +0900

    [update]twitterのアカウントで認証を行うためにmodelに記述を追加 #3
    modelにtwitterアカウントでログインを行えるように記述を追加。
    また、ユーザー分析をするために":trackable"の記述も追加した。

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:twitter]
   include DeviseTokenAuth::Concerns::User
 end
