f6f9ee5ba50149b50337a30bf012fbbf00503059
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-09 18:31:55 +0900

    [update]twitter apiを使用するためにルーティングを設定 #3
    twitter apiを使用するための記述をroutes.rbに追加。
    controllerのファイルはこの後作成し、記述する。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
+      }
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#about'
 end
