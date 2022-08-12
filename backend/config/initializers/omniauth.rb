ba40bf154a7d7965b5af363026b02a157ce1d90e
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-12 16:49:09 +0900

    [remove]Twitter api関連の不要になったファイルを削除 #3
    Twitter apiを使用しないことになったため、不要になったファイルを削除。

--- backend/config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV.fetch("TWITTER_KEY"), ENV.fetch("TWITTER_SECRET"), callback_url: "http://3000/omniauth/twitter/callback"
-end
