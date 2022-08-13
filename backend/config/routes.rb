39d78c3436f14fa02e4d01e8709f35b846504d3d
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-13 18:50:17 +0900

    [update]devise_token_auth用にルーティングを記述 #3
    アカウント作成とログイン状態確認のために必要なルーティングを追加。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
+        registrations: 'api/v1/auth/registrations'
       }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#about'
 end
