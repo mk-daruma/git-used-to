65403b6cacbdff28a5484167ace1d8523872ebe4
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-13 18:46:31 +0900

    [update]omniauth関連の記述を削除 #3
    twitter apiを使用しないことに伴い、omniauth関連の記述を削除。

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,8 +3,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:twitter]
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
