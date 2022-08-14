e115aa942185a765af11a94e4d41ed13032610a4
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-14 16:47:45 +0900

    [update]開発の効率を上げるために型定義を行なった #3
    実装時に型宣言を行う手間を省くために、src/interface/index.ts内で型宣言を行なった。

---
+++ frontend/app/src/interfaces/index.ts
@@ -0,0 +1,26 @@
+export interface SignUpParams {
+  user_name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+  user_self_introduction: string
+}
+
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+export interface User {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  user_name: string
+  user_self_introduction: string
+  nickname?: string
+  image?: string
+  allowPasswordChange: boolean
+  created_at: Date
+  updated_at: Date
+}
