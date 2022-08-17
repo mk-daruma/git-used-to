33d4952b3599fc5befd68f8f59334a800e3074c3
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-18 01:08:50 +0900

    [update]userモデルの保存処理のテストコードを記述 #3
    signup時にフォームに値が全て入っている場合は成功し、
    必須項目に値が入っていない場合や、emailがuniqueでない場合は失敗することを確認するテストを記述。
    間違ったテストを記述して失敗することを確認した後、修正してテストが通過したことも確認済み。

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates presence" do
+    context "全てのカラムに値が入力されている場合" do
+      let(:user) { create(:user) }
+
+      it "userのレコード作成が成功すること" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "emailのみ値が入力されていない場合" do
+      let(:user) { build(:user, email: nil) }
+
+      it "エラーになること" do
+        user.valid?
+        expect(user.errors.messages[:email]).to include "can't be blank"
+      end
+    end
+
+    context "passwordのみ値が入力されていない場合" do
+      let(:user) { build(:user, password: nil) }
+
+      it "エラーになること" do
+        user.valid?
+        expect(user.errors.messages[:password]).to include "can't be blank"
+      end
+    end
+  end
+
+  describe "validates uniqueness" do
+    context "保存されたメールアドレスを使用して新規ユーザーが値を入力した場合" do
+      let(:user1) { create(:user) }
+      let(:user2) { build(:user, email: user1.email) }
+
+      it "エラーになること" do
+        user2.valid?
+        expect(user2.errors.messages[:email]).to include "has already been taken"
+      end
+    end
+  end
 end
