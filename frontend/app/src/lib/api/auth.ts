e1945acab0632160dff9190433a998890ab74977
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-22 01:23:10 +0900

    [fix]コンパイルエラーを修正 #5
    サインイン/アウトに必要なtoken情報の箇所でコンパイルエラーが起きていたため、
    ユニオン型を使用し修正。

--- frontend/app/src/lib/api/auth.ts
+++ frontend/app/src/lib/api/auth.ts
@@ -13,17 +13,17 @@ export const signIn = (params: SignInParams)  => {
 
 export const signOut = () => {
   return client.delete("auth/sign_out", { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+    "access-token": Cookies.get("_access_token") || "",
+    "client": Cookies.get("_client") || "",
+    "uid": Cookies.get("_uid") || ""
   }})
 }
 
 export const getCurrentUser = () => {
   if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
   return client.get("/auth/sessions", { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+    "access-token": Cookies.get("_access_token") || "",
+    "client": Cookies.get("_client") || "",
+    "uid": Cookies.get("_uid") || ""
   }})
 }
