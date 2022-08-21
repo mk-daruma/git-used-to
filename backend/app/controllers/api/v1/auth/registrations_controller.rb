c2397c313003056b0297e2f136faa3a2fdd39e90
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-22 01:17:36 +0900

    [fix]user情報の登録時に発生していたエラーを修正 #5
    ストロングパラメータにrequireを記述していなかったためエラーが発生していた。
    セキュリティリスクを無くすため、記述を追加。

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
   def sign_up_params
-    params.permit(:email, :password, :password_confirmation, :user_name)
+    params.require(:registration).permit(:email, :password, :password_confirmation, :user_name)
   end
 end
