060e89f73bcf47429827ba389a63e905a8ff4c3a
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-25 23:57:16 +0900

    [update]user更新情報をrailsが受け取るための記述を追加 #5
    userのcontrollerでaxiosからデータを受け取るための記述を追加。

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,27 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def edit
   end
 
   def update
+    @user.user_name = user_params[:user_name]
+    @user.user_self_introduction = user_params[:user_self_introduction]
+
+    if @user.save
+      render json: { status: 200, data: @user }
+    else
+      render json: { status: 500, message: '更新に失敗しました' }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:user_name, :user_self_introduction, :id)
   end
 end
