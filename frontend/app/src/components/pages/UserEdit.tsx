754ef861031d0ec9eb56fdf6c7bde9b4d590705e
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-08-29 11:03:44 +0900

    [update]user編集画面に画像をアップロードできる機能を追加 #5
    画像アップロードの機能だけでいえば、サインアップ時の記述と同じ内容なので分割してファイルを作るべきかと考えたが、
    今後user編集画面のレイアウトを変えていく予定なので一旦分割せずに進める。

--- frontend/app/src/components/pages/UserEdit.tsx
+++ frontend/app/src/components/pages/UserEdit.tsx
@@ -1,9 +1,14 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useCallback } from "react"
 import { useHistory } from "react-router-dom";
 
 import TextField from "@material-ui/core/TextField";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button"
+import { Box } from "@material-ui/core";
+import CancelIcon from "@material-ui/icons/Cancel"
+import { IconButton } from "@material-ui/core";
+import PhotoCamera from "@material-ui/icons/PhotoCamera"
+
 
 import { AuthContext } from "App"
 import { updateUser } from "lib/api/users";
@@ -16,6 +21,18 @@ const useStyles = makeStyles((theme: Theme) =>
         margin: theme.spacing(1),
         width: "25ch"
       }
+    },
+    preview: {
+      width: "100%"
+    },
+    box: {
+      marginBottom: "1.5rem"
+    },
+    imageUploadBtn: {
+      textAlign: "right"
+    },
+    input: {
+      display: "none"
     }
   })
 );
@@ -28,12 +45,25 @@ const UserEdit: React.FC = () => {
 
   const [name, setName] = useState<string | undefined>(currentUser?.userName)
   const [introduction, setIntroduction] = useState<string | undefined>(currentUser?.userSelfIntroduction)
+  const [image, setImage] = useState<string>("")
+  const [preview, setPreview] = useState<string>("")
+
+  const uploadImage = useCallback((e :any) => {
+    const file = e.target.files[0]
+    setImage(file)
+  }, [])
+
+  const previewImage = useCallback((e :any) => {
+    const file = e.target.files[0]
+    setPreview(window.URL.createObjectURL(file))
+  }, [])
 
   const createFormData = () => {
     const formData = new FormData()
 
     formData.append("userName", name || "")
     formData.append("userSelfIntroduction", introduction || "")
+    formData.append("image", image)
 
     return formData
   }
@@ -88,6 +118,46 @@ const UserEdit: React.FC = () => {
           setIntroduction(e.target.value)
         }}
       />
+      <div className={classes.imageUploadBtn}>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="icon-button-file"
+          type="file"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            uploadImage(e)
+            previewImage(e)
+          }}
+        />
+        <label htmlFor="icon-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <PhotoCamera />
+          </IconButton>
+        </label>
+      </div>
+      {
+        preview ? (
+          <Box
+            className={classes.box}
+          >
+            <IconButton
+              color="inherit"
+              onClick={() => setPreview("")}
+            >
+              <CancelIcon />
+            </IconButton>
+            <img
+              src={preview}
+              alt="preview img"
+              className={classes.preview}
+            />
+          </Box>
+        ) : null
+      }
       <Button
         onClick={handleFormSubmit}
         disabled={!name || !introduction ? true : false}
