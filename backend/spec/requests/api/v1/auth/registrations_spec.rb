8a7ba129c7ceae01bd67d7994033ca1ce9f4b647
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-09-05 15:24:30 +0900

    [update]パスワード変更のテストを追加 #5
    パスワードを用意した別のパスワードに変更できるテストを追加した。
    また、アカウントデータ削除のmessageの文字制限の指摘対象から除外する記述も併せて追加した。
    できるだけ対象除外は使用しない方がいいかもしれないが、変数を作って代入などの段階を踏む方が可読性が下がると考えたため追加した。

--- backend/spec/requests/api/v1/auth/registrations_spec.rb
+++ backend/spec/requests/api/v1/auth/registrations_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "POST /api/v1/auth" do
     let(:url) { "sample.url" }
 
-    context "認証済みで正しい値が入力されている場合" do
+    context "正しい値が入力されている場合" do
       let(:params) { attributes_for(:user, confirm_success_url: [url]) }
 
       it "ユーザー登録が成功すること" do
@@ -62,7 +62,23 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
         delete api_v1_user_registration_path, headers: headers
         res = JSON.parse(response.body)
         expect(res["status"]).to eq("success")
-        expect(res["message"]).to include ("Account with UID '#{current_user.uid}' has been destroyed.")
+        expect(res["message"]).to include("Account with UID '#{current_user.uid}' has been destroyed.") # rubocop:disable LineLength
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "userデータが存在する場合" do
+      let(:current_user) { create(:user) }
+      let(:current_user2) { create(:user) }
+      let(:params) { { password: current_user.password = current_user2.password } }
+      let(:headers) { current_user.create_new_auth_token }
+
+      it "userデータのパスワード変更が成功すること" do
+        expect(current_user.password).not_to eq(current_user2.password)
+        put api_v1_user_registration_path, params: params, headers: headers
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("success")
+        expect(current_user.password).to eq(current_user2.password)
         expect(response).to have_http_status(200)
       end
     end
