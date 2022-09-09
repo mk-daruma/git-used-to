866b8b2fcbcc11c6ca74eba03a64e9445de28c52
mk-daruma <mk69sh@gmail.com>
2022-09-09 17:17:27 +0900

    [clean]api通信用関数を記述する箇所を間違えていたため正しいファイルに移動 #7
    auth(認証)に関してのapi関数を格納しているファイルに、
    userの情報を変更する関数(パスワード変更や削除など)の関数が記述されていたため、
    users.tsに関数を移動した。

--- frontend/app/src/lib/api/auth.ts
+++ frontend/app/src/lib/api/auth.ts
@@ -1,7 +1,7 @@
 import client from "lib/api/client"
 import Cookies from "js-cookie"
 
-import { SignUpFormData, SignInParams, ChangeUserPasswordFormData, ForgetUserPasswordFormData } from "interfaces/index" //RedirectChangeUserPasswordDataFormData
+import { SignUpFormData, SignInParams } from "interfaces/index"
 
 export const signUp = (data: SignUpFormData) => {
   return client.post("auth", data)
@@ -22,26 +22,6 @@ export const signOut = () => {
 export const getCurrentUser = () => {
   if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
   return client.get("/auth/sessions", { headers: {
-    "access-token": Cookies.get("_access_token") || "",
-    "client": Cookies.get("_client") || "",
-    "uid": Cookies.get("_uid") || ""
-  }})
-}
-
-export const changeCurrentUserPassword = (data: ChangeUserPasswordFormData) => {
-  return client.put("auth", data, { headers: {
-    "access-token": Cookies.get("_access_token") || "",
-    "client": Cookies.get("_client") || "",
-    "uid": Cookies.get("_uid") || ""
-  }})
-}
-
-export const forgetCurrentUserPassword = (data: ForgetUserPasswordFormData) => {
-  return client.post("auth/password", data)
-}
-
-export const deleteUser = () => {
-  return client.delete("auth", { headers: {
     "access-token": Cookies.get("_access_token") || "",
     "client": Cookies.get("_client") || "",
     "uid": Cookies.get("_uid") || ""
