dcbe3bb614de3e356de7b1646722dc2acb292f8d
mk-daruma <mk69sh@gmail.com>
2022-09-09 12:14:15 +0900

    [update]quiz_branchのcontrollerとルーティングを記述 #7
    showアクションでは、quiz_commit_messagesテーブルと、quiz_worktree_filesテーブルの関連データを取得してjsonで送るように記述した。
    別々のアクションを作成するか考えたが、一旦ルーティングをresourcesの記述のみでまとめたいと考えたため、上記のような記述内容にした。
    実装していくなかで別々のアクションの方が都合が良いと思い変更する可能性もある。

--- backend/app/controllers/api/v1/quiz_branches_controller.rb
+++ backend/app/controllers/api/v1/quiz_branches_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::QuizBranchesController < ApplicationController
+  before_action :set_quiz_branch, only: [:show, :update, :destroy]
+  before_action :quiz_branch_params, only: [:create, :update]
+
+  def create
+    quiz_branch = QuizBranch.new(quiz_branch_params)
+    if quiz_branch.save
+      render json: { status: 'SUCCESS', data: quiz_branch }
+    else
+      render json: quiz_branch.errors
+    end
+  end
+
+  def show
+    quiz_worktree_files = @quiz_branch.quiz_worktree_files
+    quiz_commit_messages = @quiz_branch.quiz_commit_messages
+    render json: { status: 'SUCCESS', message: 'Loaded quizzes', data_worktrees: quiz_worktree_files, data_messages: quiz_commit_messages }
+  end
+
+  def update
+    if @quiz_branch.update(quiz_branch_params)
+      render json: { status: 'SUCCESS', data: @quiz_branch }
+    else
+      render json: { status: 'SUCCESS', message: 'Not updated', data: @quiz_branch.errors }
+    end
+  end
+
+  def destroy
+    quiz_branch = @quiz_branch.destroy
+    render json: { status: 'SUCCESS', message: 'Deleted the post', data: quiz_branch }
+  end
+
+  private
+
+  def set_quiz_branch
+    @quiz_branch = QuizBranch.find(params[:id])
+  end
+
+  def quiz_branch_params
+    params.require(:quiz_branch).permit(:quiz_branch_name, :quiz_first_or_last_id)
+  end
 end
