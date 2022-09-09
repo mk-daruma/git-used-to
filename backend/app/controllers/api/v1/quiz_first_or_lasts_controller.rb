aa021517510f008523b52818752675ab06575ef8
mk-daruma <mk69sh@gmail.com>
2022-09-09 12:15:14 +0900

    [update]quiz_first_or_lastのupdateアクションの記述内容を修正 #7
    quiz_first_or_lastの情報の更新ができなかった際のjsonのdataに関しての記述が間違っていたため修正。

--- backend/app/controllers/api/v1/quiz_first_or_lasts_controller.rb
+++ backend/app/controllers/api/v1/quiz_first_or_lasts_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::QuizFirstOrLastsController < ApplicationController
     if @quiz_first_or_last.update(quiz_first_or_last_params)
       render json: { status: 'SUCCESS', data: @quiz_first_or_last }
     else
-      render json: { status: 'SUCCESS', message: 'Not updated', data: quiz_first_or_last.errors }
+      render json: { status: 'SUCCESS', message: 'Not updated', data: @quiz_first_or_last.errors }
     end
   end
 
