3fa19ed45cbceebdf39765ad77ca53deb4b5923e
mk-daruma <mk69sh@gmail.com>
2022-09-22 23:01:57 +0900

    [update]URLからidを取得するための記述を追加 #7
    useParamsでidに紐づいたクイズの情報を取得するために、pathに":id"を追加。

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -82,6 +82,7 @@ const App: React.FC = () => {
                 <Route exact path="/user/delete" component={UserDelete} />
                 <Route exact path="/user/quizzes" component={UserQuizzes} />
                 <Route exact path="/quiz" component={CreateQuiz} />
+                <Route path="/quiz/edit/:id" component={CreateQuiz} />
               </Switch>
             </Private>
           </Switch>
