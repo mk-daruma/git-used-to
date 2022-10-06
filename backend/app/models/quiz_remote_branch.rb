c1dc226431a249ffbfb7908221e98da5054367d6
mk-daruma <mk69sh@gmail.com>
2022-10-06 12:40:22 +0900

    [update]モデルにアソシエーションを追加 #7
    リモートリポジトリ関連のmodelにアソシエーションの記述を追加。

--- backend/app/models/quiz_remote_branch.rb
+++ backend/app/models/quiz_remote_branch.rb
@@ -1,3 +1,4 @@
 class QuizRemoteBranch < ApplicationRecord
   belongs_to :quiz_first_or_last
+  has_many :quiz_Remote_commit_messages
 end
--- backend/app/models/quiz_remote_commit_message.rb
+++ backend/app/models/quiz_remote_commit_message.rb
@@ -1,3 +1,4 @@
 class QuizRemoteCommitMessage < ApplicationRecord
   belongs_to :quiz_remote_branch
+  has_many :quiz_remote_repository_files
 end
