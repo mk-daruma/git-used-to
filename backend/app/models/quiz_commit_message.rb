28c7fe479d7779972a43e045c4dd24de21b7da69
mk-daruma <mk69sh@gmail.com>
2022-10-15 16:17:10 +0900

    [fix]間違った関連づけの記述を修正 #7
    quiz_branchとquiz_commit_messageのmodelファイル内でquiz_history_of_committed_fileへの関連づけの記述が間違っていたため修正。

--- backend/app/models/quiz_commit_message.rb
+++ backend/app/models/quiz_commit_message.rb
@@ -1,5 +1,5 @@
 class QuizCommitMessage < ApplicationRecord
   belongs_to :quiz_branch
-  has_many :quiz_remote_repository_files
+  has_many :quiz_history_of_committed_files
   has_many :quiz_repository_files
 end
