fb3391c7391caca4848412bf01ad9ebb9d6b23eb
mk-daruma <mk69sh@gmail.com>
2022-10-16 19:28:41 +0900

    [update]api用関数の関数名と通信するurlを修正 #7
    QuizRemoteRepositoryFileと記述すべき箇所が"Remote"が抜けて記述がされていたため、修正。

--- frontend/app/src/lib/api/quiz_remote_repository_file.ts
+++ frontend/app/src/lib/api/quiz_remote_repository_file.ts
@@ -1,16 +1,16 @@
 import { CreateQuizRemoteRepositoryFileData } from "interfaces"
 import client from "lib/api/client"
 
-export const createQuizRepositoryFile = (data: CreateQuizRemoteRepositoryFileData) => {
+export const createQuizRemoteRepositoryFile = (data: CreateQuizRemoteRepositoryFileData) => {
   if (Object.keys(data).length > 0) {
-    return client.post("quiz_repository_files", data)
+    return client.post("quiz_remote_repository_files", data)
   }
 }
 
-// export const updateQuizRepositoryFile = (id: number | undefined | null, data: これから作成) => {
-//   return client.post(`quiz_repository_files/${id}`, data)
+// export const updateQuizRemoteRepositoryFile = (id: number | undefined | null, data: これから作成) => {
+//   return client.post(`quiz_remote_repository_files/${id}`, data)
 // }
 
-export const deleteQuizRepositoryFile = (id: number | undefined | null) => {
-  return client.delete(`quiz_repository_files/${id}`)
+export const deleteQuizRemoteRepositoryFile = (id: number | undefined | null) => {
+  return client.delete(`quiz_remote_repository_files/${id}`)
 }
