ece573c7fffecad0ee5ba608c815935acfc2a571
mk-daruma <mk69sh@gmail.com>
2022-10-16 19:27:07 +0900

    [fix]タイポ修正 #7
    関連付けの記述にタイポがあったため修正。

--- backend/app/models/quiz_remote_branch.rb
+++ backend/app/models/quiz_remote_branch.rb
@@ -1,5 +1,5 @@
 class QuizRemoteBranch < ApplicationRecord
   belongs_to :quiz_first_or_last
-  has_many :quiz_Remote_commit_messages
+  has_many :quiz_remote_commit_messages
   has_many :quiz_remote_repository_files
 end
