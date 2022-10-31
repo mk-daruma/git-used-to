f5263d9cfd42a4d419246a1a62fa7a71df9bd399
mk-daruma <mk69sh@gmail.com>
2022-10-31 13:26:01 +0900

    [update]クイズ一覧で問題の回答開始状態の作成や編集を行えるように変更 #7
    デザイン面などは今後修正するが、一旦自分が作成したクイズの正解の状態だけでなく、解答開始の状態を作成/編集できるように変更。

--- frontend/app/src/components/pages/UserQuizzes.tsx
+++ frontend/app/src/components/pages/UserQuizzes.tsx
@@ -1,9 +1,21 @@
+import { Button, makeStyles, Theme } from "@material-ui/core";
 import { AuthContext } from "App";
 import { getUserQuizzes } from "lib/api/users"
 import React, { useContext, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  submitBtn: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+    textTransform: "none",
+    backgroundColor: "#f5f5f5"
+  }
+}))
 
 const UserQuizzes: React.FC = () => {
   const { currentUser } = useContext(AuthContext)
+  const classes = useStyles()
   const [userQuizzes, setUserQuizzes] = useState([{
     id: "",
     quizTitle: "",
@@ -26,6 +38,7 @@ const UserQuizzes: React.FC = () => {
 
   useEffect(() => {
     handleGetUserQuizzes()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -35,6 +48,42 @@ const UserQuizzes: React.FC = () => {
         <p>{ userQuiz.id }</p>
         <p>{ userQuiz.quizTitle }</p>
         <p>{ userQuiz.quizIntroduction }</p>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          fullWidth
+          color="default"
+          className={classes.submitBtn}
+          component={Link}
+          to={`/quiz/edit/${userQuiz.id}`}
+        >
+          編集
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          fullWidth
+          color="default"
+          className={classes.submitBtn}
+          component={Link}
+          to={`/quiz/init/${userQuiz.id}`}
+        >
+          初期値を設定
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          fullWidth
+          color="default"
+          className={classes.submitBtn}
+          component={Link}
+          to={`/quiz/init/edit/${userQuiz.id}`}
+        >
+          初期値を編集
+        </Button>
       </>
       ))}
     </>
