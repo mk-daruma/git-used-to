9befd5543f01e59865e68ff9ec1d9a7c9206349f
mk-daruma <mk69sh@gmail.com>
2022-11-03 00:38:04 +0900

    [update]userのshowアクションでquiz_answer_recordのデータを取得するテストを記述 #9
    user/showでアクションでquiz_answer_recordsのデータを取得が成功するか確認するテストを追加。

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -9,11 +9,16 @@ RSpec.describe "Api::V1::Users", type: :request do
   let!(:user_quiz2) { create(:quiz, user: user) }
   let!(:user2_quiz1) { create(:quiz, user: user2) }
   let!(:user2_quiz2) { create(:quiz, user: user2) }
+  let!(:related_quiz_answer_record) { create(:quiz_answer_record, user: user, quiz: user2_quiz1) }
+  let!(:not_related_quiz_answer_record) { create(:quiz_answer_record) }
 
   describe "GET /api/v1/users#show" do
     context "引数がquizのidの場合" do
-      it "userの関連しているquizデータのみを取得できること" do
+      before do
         get api_v1_user_path(user.id)
+      end
+
+      it "userの関連しているquizデータのみを取得できること" do
         res = JSON.parse(response.body)
         expect(res["status"]).to eq("SUCCESS")
         expect(res["message"]).to eq("Loaded quizzes")
@@ -22,6 +27,15 @@ RSpec.describe "Api::V1::Users", type: :request do
         expect(res["data"][0]["id"]).not_to eq(user2_quiz1.id)
         expect(res["data"][1]["id"]).not_to eq(user2_quiz2.id)
         expect(res["data"].length).to eq 2
+        expect(response).to have_http_status(:success)
+      end
+
+      it "userの関連しているquiz_answer_recordデータのみを取得できること" do
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("SUCCESS")
+        expect(res["message"]).to eq("Loaded quizzes")
+        expect(res["data_answer_records"][0]["id"]).to eq(related_quiz_answer_record.id)
+        expect(res["data_answer_records"].length).to eq 1
         expect(response).to have_http_status(:success)
       end
     end
