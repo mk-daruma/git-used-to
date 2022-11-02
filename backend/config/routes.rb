1500f4019229662767a8c4f1ff45a0aadba9e2b8
mk-daruma <mk69sh@gmail.com>
2022-11-02 12:12:23 +0900

    [add]quiz_answer_recordsのcontrollerファイルを作成 #9
    クイズが解答済みかを判別するためのcontrollerファイルを作成し、ルーティングを記述。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
       resources :users, only: %i[show update]
       resources :quizzes, only: %i[index create update show destroy]
+      resources :quiz_answer_records, only: %i[create]
       resources :quiz_first_or_lasts, only: %i[create show destroy]
       resources :quiz_branches, only: %i[create show update destroy]
       resources :quiz_remote_branches, only: %i[create show destroy]
