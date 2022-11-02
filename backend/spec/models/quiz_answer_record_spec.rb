d67b2ebf81079465b9d400afeb65c7ae9a56183c
mk-daruma <mk69sh@gmail.com>
2022-11-03 00:47:45 +0900

    [update]quiz_answer_recordsのmodelテストを行わない理由を記述 #9
    quiz_answer_recordsのmodelは外部キーのアソシエーションのみの記述なので、
    テストを行う項目がないと考え、上記の理由をテストファイルにpendingでテスト中に表示されるように記述。

--- backend/spec/models/quiz_answer_record_spec.rb
+++ backend/spec/models/quiz_answer_record_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe QuizAnswerRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "quiz_answer_recordモデルには、外部キーの設定のみのためテストを書いていない。"
 end
