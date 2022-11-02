988ec49e64a3a6478b664e7ab5272be2264ae50a
mk-daruma <mk69sh@gmail.com>
2022-11-02 15:41:54 +0900

    [update]quiz_answer_recordsのcreateアクションを追加 #9
    quiz_answer_recordsのデータの取得はuser_controllerから、削除は親モデルのuserかquizが削除された際に同時に削除されるため、
    このファイルのアクションはcreateのみ記述している。

--- backend/app/controllers/api/v1/quiz_answer_records_controller.rb
+++ backend/app/controllers/api/v1/quiz_answer_records_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::QuizAnswerRecordsController < ApplicationController
+  before_action :quiz_answer_record_params, only: [:create]
+
   def create
+    quiz_answer_records = QuizAnswerRecord.new(quiz_answer_record_params)
+    if quiz_answer_records.save!
+      render json: {
+        status: 'SUCCESS',
+        data: quiz_answer_records,
+      }
+    else
+      render json: quiz_answer_records.errors
+    end
+  end
+
+  private
+
+  def quiz_answer_record_params
+    params.require(:quiz_answer_record).permit(:user_id, :quiz_id)
   end
 end
