e2b546dbca4b5b9ac90e509609cc0dd98f957057
mk-daruma <mk69sh@gmail.com>
2022-11-04 22:28:10 +0900

    [update]保持するデータが外部キーのみの2つのmodelで存在している値の保存が失敗するテストを記述 #15
    validates_uniqueness_ofの記述をしている2つのmodelで、
    同じ値が既にテーブル内に存在する場合に保存が失敗するテストを記述。

--- backend/spec/models/quiz_answer_record_spec.rb
+++ backend/spec/models/quiz_answer_record_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe QuizAnswerRecord, type: :model do
-  pending "quiz_answer_recordモデルには、外部キーの設定のみのためテストを書いていない。"
+  let!(:user) { create(:user) }
+  let!(:quiz) { create(:quiz) }
+  let!(:quiz_answer_record) { create(:quiz_answer_record, user: user, quiz: quiz) }
+
+  describe "validates presence" do
+    context "同じ値のquiz_answer_recordのデータが存在しない場合" do
+      it "quizのレコード作成が成功すること" do
+        expect(quiz_answer_record).to be_valid
+      end
+    end
+
+    context "既に同じ値のquiz_answer_recordのデータが存在する場合" do
+      let(:quiz_answer_record2) { build(:quiz_answer_record, user: user, quiz: quiz) }
+
+      it "エラーになること" do
+        quiz_answer_record2.valid?
+        expect(quiz_answer_record2.errors.messages[:quiz_id]).to include "has already been taken"
+      end
+    end
+  end
 end
