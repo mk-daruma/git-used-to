8321ea0794c22bc4c0abb718c24d1aaf5ea5c500
mk-daruma <mk69sh@gmail.com>
2022-11-04 20:33:22 +0900

    [add]ブックマーク用のcontrollerファイルを作成 #15
    ブックマーク用のcontrollerファイルを作成し、同時にrequest_specファイルを作成。

---
+++ backend/spec/requests/api/v1/quiz_bookmarks_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::QuizBookmarks", type: :request do
+  describe "GET /create" do
+    it "returns http success" do
+      get "/api/v1/quiz_bookmarks/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/api/v1/quiz_bookmarks/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
