16e7cbe0452f3fd4f9bf6841b25029884d7c3e0e
mk-daruma <mk69sh@gmail.com>
2022-11-04 22:24:29 +0900

    [update]quiz_bookmarkが既に同じ値が存在している場合保存が失敗する様にmodelに記述を追加 #15
    カラムが外部データのみの2つのテーブルに関して、
    controllerだけでなくmodelでも同じデータを保存できないように処理を追加。

--- backend/app/models/quiz_answer_record.rb
+++ backend/app/models/quiz_answer_record.rb
@@ -1,4 +1,6 @@
 class QuizAnswerRecord < ApplicationRecord
   belongs_to :user
   belongs_to :quiz
+
+  validates_uniqueness_of :quiz_id, scope: :user_id
 end
--- backend/app/models/quiz_bookmark.rb
+++ backend/app/models/quiz_bookmark.rb
@@ -1,4 +1,6 @@
 class QuizBookmark < ApplicationRecord
   belongs_to :quiz
   belongs_to :user
+
+  validates_uniqueness_of :quiz_id, scope: :user_id
 end
