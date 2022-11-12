a44850306b6ff2dfc7d13bb5315fd6f26cca9b80
mk-daruma <mk69sh@gmail.com>
2022-11-12 13:05:47 +0900

    [update]コメント削除ボタンを追加 #17
    コメントを削除するボタンを実装。
    ボタンが押された際に指定されたコメントがDBから削除され、それと同時にquizCommentHistoryからも削除される

--- frontend/app/src/components/pages/QuizComment.tsx
+++ frontend/app/src/components/pages/QuizComment.tsx
@@ -1,6 +1,7 @@
 import { Button, createStyles, makeStyles, TextField, Theme } from "@material-ui/core"
+import DeleteIcon from '@material-ui/icons/Delete';
 import { AuthContext } from "App"
-import { createQuizComment } from "lib/api/quiz_comments"
+import { createQuizComment, deleteQuizComment } from "lib/api/quiz_comments"
 import { useContext } from "react"
 import { QuizBookmarkContext } from "./QuizList"
 
@@ -35,21 +36,29 @@ const QuizComment: React.FC<{ quizId: number }> = ({quizId}) => {
         comment: quizComment
       }
       const quizCommentRes = await createQuizComment(quizCommentData)
-
       setQuizCommentHistory((commentHistory) => [...commentHistory,{
         id: quizCommentRes.data.data.id,
         quizId: quizCommentRes.data.data.quizId,
         userId:quizCommentRes.data.data.userId,
         comment: quizCommentRes.data.data.comment
       }])
-
       setQuizComment("")
-
       console.log("quizCommentRes", quizCommentRes)
     } catch (err) {
       console.log(err)
     }
   }
+
+  const handleDeleteQuizCommentSubmit = async(e: React.MouseEvent<HTMLButtonElement>,commentId :any) => {
+    try {
+      deleteQuizComment(commentId)
+      setQuizCommentHistory(quizCommentHistory.filter(comment => comment.id !== commentId))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const deleteComment = quizCommentHistory.filter(commentHistory =>commentHistory.quizId === quizId)
 
   return(
     <>
@@ -69,7 +78,6 @@ const QuizComment: React.FC<{ quizId: number }> = ({quizId}) => {
         type="submit"
         variant="contained"
         size="large"
-        fullWidth
         color="default"
         disabled={!quizComment}
         className={classes.submitBtn}
@@ -77,6 +85,21 @@ const QuizComment: React.FC<{ quizId: number }> = ({quizId}) => {
         >
         Submit
       </Button>
+      {deleteComment.map(comment =>
+        <p>
+          {comment.comment}
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            color="default"
+            className={classes.submitBtn}
+            onClick={e => handleDeleteQuizCommentSubmit(e, comment.id)}
+            >
+            <DeleteIcon />
+          </Button>
+        </p>
+        )}
     </>
   )
 }
