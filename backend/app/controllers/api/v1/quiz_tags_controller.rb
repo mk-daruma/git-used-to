1345e18c8564650c87679c91c246bbaac145b18d
mk-daruma <mk69sh@gmail.com>
2022-11-13 22:02:28 +0900

    [update]タグ機能用のcontrollerファイルにアクションを追加 #19
    タグ機能用のcontrollerファイルに、create/destroyアクションを追加。

--- backend/app/controllers/api/v1/quiz_tags_controller.rb
+++ backend/app/controllers/api/v1/quiz_tags_controller.rb
@@ -1,7 +1,35 @@
 class Api::V1::QuizTagsController < ApplicationController
+  before_action :quiz_tag_params, only: [:create]
+  before_action :set_quiz_tag, only: [:destroy]
+
   def create
+    quiz_tags = QuizTag.new(quiz_tag_params)
+    if quiz_tags.save
+      render json: {
+        status: 'SUCCESS',
+        data: quiz_tags,
+      }
+    else
+      render json: quiz_tags.errors
+    end
   end
 
   def destroy
+    quiz_tag = @quiz_tag.destroy
+    render json: {
+      status: 'SUCCESS',
+      message: 'Deleted the quiz tag',
+      data: quiz_tag,
+    }
+  end
+
+  private
+
+  def set_quiz_tag
+    @quiz_tag = QuizTag.find(params[:id])
+  end
+
+  def quiz_tag_params
+    params.require(:quiz_tag).permit(:quiz_id, :tag)
   end
 end
