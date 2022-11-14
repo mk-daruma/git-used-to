cb55b6c4bfc53ca55a458a98c8c2c14bd4ff78f3
mk-daruma <mk69sh@gmail.com>
2022-11-14 22:51:30 +0900

    [update]quiz_tagのテストを記述 #19
    models/requestsのテストを記述。
    requestsのテストではjsonのデータ数もチェックしている。

--- backend/spec/models/quiz_tag_spec.rb
+++ backend/spec/models/quiz_tag_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe QuizTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def err_message(karam)
+    quiz_tag.errors.messages[karam]
+  end
+
+  describe "validates presence" do
+    context "tagが空の場合" do
+      let(:quiz_tag) { build(:quiz_tag, tag: "") }
+
+      it "エラーになること" do
+        quiz_tag.valid?
+        expect(err_message(:tag)).to include "can't be blank"
+      end
+    end
+  end
 end
