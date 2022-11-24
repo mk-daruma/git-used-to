e593ca09fe56dd983623b2f44cf9571d538567fa
mk-daruma <mk69sh@gmail.com>
2022-11-24 15:43:30 +0900

    [update]ゲストログイン用のデータをseedに記述 #41
    ゲストログイン用のuserデータをseed.rbに記述。
    作成したuserでログインできることを確認済み。

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  user_name: 'guest user',
+  user_self_introduction: "これは自己紹介用のフォームです。ぜひアカウントを作成してご自身の自己紹介をしてみてください！",
+  email: ENV['GEST_USER_ADRESS'],
+  confirmed_at: Date.today,
+  password: ENV['GEST_USER_PASSWORD']
+)
