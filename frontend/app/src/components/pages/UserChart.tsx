c2f754ed3a06441d43bfe62dd92420bc1a2f99c9
mk-daruma <mk69sh@gmail.com>
2022-12-08 00:32:00 +0900

    [update]userの実績をチャートとして表示する記述を追加 #34
    userのプロフィール画面でクイズ作成数/回答数/ブックマークされた数をチャート化して表示されるように記述を追加。
    全体の平均値も表示して比較ができるように記述している。

---
+++ frontend/app/src/components/pages/UserChart.tsx
@@ -0,0 +1,67 @@
+import { AuthContext } from "App";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { useContext } from "react";
+import { Bar } from "react-chartjs-2";
+import { UserProfileContext } from "./UserProfile";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const UserChart: React.FC = () => {
+  const { currentUser } = useContext(AuthContext)
+  const { userProfile } = useContext(UserProfileContext)
+  const options = {
+    responsive: false,
+    plugins: {
+      legend: {
+        position: "top" as const
+      },
+      title: {
+        display: true,
+        text: `${currentUser?.userName}さんの記録`
+      }
+    }
+  };
+
+  const labels = ["クイズ作成数", "解答したクイズの数", "ブックマークされた数"];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: `${currentUser?.userName}さん`,
+        data: [userProfile.quizzesLength, userProfile.answerRecordsLength, userProfile.bookmarksLength],
+        backgroundColor: "rgba(255, 99, 132, 0.5)"
+      },
+      {
+        label: "全体の平均数",
+        data: [userProfile.quizAverage, userProfile.answerRecordAverage, userProfile.bookmarkAverage],
+        backgroundColor: "rgba(53, 162, 235, 0.5)"
+      }
+    ]
+  };
+
+  return(
+    <Bar
+      height={600}
+      width={600}
+      options={options}
+      data={data}
+      />
+  )
+}
+
+export default UserChart
