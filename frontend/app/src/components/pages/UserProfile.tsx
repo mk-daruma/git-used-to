0ce95de1a7682e7b89d80c1e35a06ac55b7e368d
mk-daruma <mk69sh@gmail.com>
2022-12-08 10:21:59 +0900

    [update]ログイン中ユーザーかゲストユーザー/他ユーザーによって表示されるプロフィール画面が変化する記述を追加 #34
    ログイン中のユーザーの場合と、ゲストユーザー/他ユーザーの場合で表示する内容(ボタンの処理やフォームに入力できないようにする等)を変化させる記述を追加。

--- frontend/app/src/components/pages/UserProfile.tsx
+++ frontend/app/src/components/pages/UserProfile.tsx
@@ -1,9 +1,8 @@
-import { Avatar, Button } from "@material-ui/core"
 import { AuthContext } from "App"
-import AvatarImage from "components/layouts/Avatar"
 import { getUserProfile } from "lib/api/users"
 import { createContext, useContext, useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import OtherUserProfile from "./OtherUserProfile"
 import UserEdit from "./UserEdit"
 
 export const UserProfileContext = createContext({} as {
@@ -11,6 +10,7 @@ export const UserProfileContext = createContext({} as {
     userId: number,
     userName: string,
     userSelfIntroduction: string,
+    userEmail: string,
     userImage: {
       url: string,
     },
@@ -27,6 +27,7 @@ export const UserProfileContext = createContext({} as {
     userId: number,
     userName: string,
     userSelfIntroduction: string,
+    userEmail: string,
     userImage: {
       url: string,
     },
@@ -45,6 +46,7 @@ const UserProfile: React.FC = () => {
     userId: 0,
     userName: "",
     userSelfIntroduction: "",
+    userEmail: "",
     userImage: {
       url: "",
     },
@@ -66,6 +68,7 @@ const UserProfile: React.FC = () => {
       userId: userProfileRes.data.userData.id,
       userName: userProfileRes.data.userData.userName,
       userSelfIntroduction: userProfileRes.data.userData.userSelfIntroduction,
+      userEmail: userProfileRes.data.userData.email,
       userImage: {
         url: userProfileRes.data.userData.image.url,
       },
@@ -76,9 +79,13 @@ const UserProfile: React.FC = () => {
       quizAverage: userProfileRes.data.quizAverage,
       answerRecordAverage: userProfileRes.data.quizAnswerRecordAverage,
       bookmarkAverage: userProfileRes.data.quizBookmarkAverage,
-
     })
   }
+
+  const checkGuestUser = currentUser?.email === "guest_user@git-used-to.com"
+  const checkCurrentUser = id === String(currentUser?.id)
+  const checkEditUser = checkCurrentUser && !checkGuestUser
+  const checkGuestOrOtherUser = !checkCurrentUser || checkGuestUser
 
   useEffect(() => {
     handleGetUserProfile()
@@ -95,40 +102,8 @@ const UserProfile: React.FC = () => {
         userProfile, setUserProfile
       }}
       >
-      {id === String(currentUser?.id) &&
-        <UserEdit />
-        }
-
-      {id !== String(currentUser?.id) &&
-        <>
-          <Button
-            type="submit"
-            color="default"
-            variant="contained"
-            component={Link}
-            to={`/user/${id}/quiz/list`}
-          >
-            userが作成したクイズ一覧
-          </Button>
-          <Button
-            type="submit"
-            color="default"
-            variant="contained"
-            component={Link}
-            to={`/user/${id}/quiz/bookmark/list`}
-          >
-            userがブックマークしたクイズ一覧
-          </Button>
-          <AvatarImage image={userProfile.userImage.url} rank={userProfile.userNickname} />
-          <p>{userProfile.userName}</p>
-          <p>{userProfile.userSelfIntroduction}</p>
-          <p>{userProfile.answerRecordsLength}</p>
-          <p>{userProfile.bookmarksLength}</p>
-          <p>{userProfile.quizzesLength}</p>
-          <p>{userProfile.userName}</p>
-          <p>{userProfile.userSelfIntroduction}</p>
-        </>
-        }
+      {checkEditUser && <UserEdit />}
+      {checkGuestOrOtherUser && <OtherUserProfile />}
     </UserProfileContext.Provider>
   )
 }
