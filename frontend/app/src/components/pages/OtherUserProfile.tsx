c816714ac218b84b83f3f776e3f80fa9edc59064
mk-daruma <mk69sh@gmail.com>
2022-12-08 10:33:34 +0900

    [update]ログインしていないユーザーとゲストユーザー用のプロフィール画面の表示内容を記述 #34
    ログイン中ユーザーとは違い、名前/自己紹介文を編集できないようにdisabledを有効にしている。
    また、ログイン中のゲストユーザーが自分のプロフィール画面を開いた場合に限りアカウント作成を案内するボタンが表示される。

---
+++ frontend/app/src/components/pages/OtherUserProfile.tsx
@@ -0,0 +1,153 @@
+import React, { useContext } from "react"
+import { Link } from "react-router-dom";
+import { Avatar, Card } from "@material-ui/core"
+import { motion } from "framer-motion"
+
+import TextField from "@material-ui/core/TextField";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button"
+import UserChart from "./UserChart";
+import { UserProfileContext } from "./UserProfile";
+import ReccomendSignUpModal from "./RecommendSignUpModal";
+import { AuthContext } from "App";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& .MuiTextField-root": {
+        marginBottom: theme.spacing(2),
+        width: "100%"
+      }
+    },
+    preview: {
+      width: "100%"
+    },
+    box: {
+      margin: "1.5rem"
+    },
+    imageUploadBtn: {
+      textAlign: "right"
+    },
+    input: {
+      display: "none"
+    },
+    link: {
+      textDecoration: "none",
+      padding: "1.5rem"
+    },
+    image: {
+      width: theme.spacing(10),
+      height: theme.spacing(10),
+    },
+    imageAndUserTitle: {
+      display: "flex",
+      alignItems: "center",
+      flexWrap: "wrap",
+      marginBottom: "1.5rem"
+    },
+    card: {
+      padding: theme.spacing(3),
+      margin: theme.spacing(6),
+      maxWidth: 1600,
+      borderRadius: "2rem",
+      display: "flex",
+      alignItems: "center"
+    },
+    btn: {
+      marginBottom: "1.5rem"
+    },
+    chart: {
+      paddingLeft: "1.5rem"
+    },
+    titleFont: {
+      fontSize: "100%"
+    }
+  })
+);
+
+const OtherUserProfile: React.FC = () => {
+  const { currentUser } = useContext(AuthContext)
+  const { userProfile } = useContext(UserProfileContext)
+  const classes = useStyles();
+
+  const checkGuestUser = userProfile.userEmail === "guest_user@git-used-to.com" && currentUser?.email === "guest_user@git-used-to.com"
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+      >
+      <Card className={classes.card}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <div className={classes.imageAndUserTitle}>
+            <div className={classes.imageUploadBtn}>
+              <Avatar
+                className={classes.image}
+                src={userProfile.userImage.url}
+                />
+            </div>
+            <div className={classes.titleFont}>
+              【現在の称号】{userProfile.userNickname}
+            </div>
+          </div>
+          <TextField
+            required
+            multiline
+            id="outlined-required"
+            disabled={true}
+            defaultValue={`${userProfile.userName}`}
+            variant="outlined"
+            />
+          <TextField
+            required
+            multiline
+            minRows="10"
+            id="outlined-required"
+            disabled={true}
+            defaultValue={userProfile.userSelfIntroduction}
+            variant="outlined"
+            />
+            {checkGuestUser
+            ? <ReccomendSignUpModal />
+            : <>
+                <Button
+                  className={classes.btn}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  color="default"
+                  component={Link}
+                  to={`/user/${userProfile.userId}/quiz/list`}
+                >
+                  {userProfile.userName}が作成したクイズ一覧
+                </Button>
+                <Button
+                  className={classes.btn}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  color="default"
+                  component={Link}
+                  to={`/user/${userProfile.userId}/quiz/bookmark/list`}
+                >
+                  {userProfile.userName}がブックマークしたクイズ一覧
+                </Button>
+              </>
+            }
+        </form>
+        <div className={classes.chart}>
+          <UserChart />
+        </div>
+      </Card>
+    </motion.div>
+  )
+}
+
+export default OtherUserProfile
