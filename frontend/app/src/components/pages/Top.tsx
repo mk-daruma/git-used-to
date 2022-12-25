c8a7c16e450bd8a5964257839a6a907ccc7201ea
mk-daruma <mk69sh@gmail.com>
2022-12-25 17:38:11 +0900

    [update]モバイルでgit used toを開いている際の処理を追加 #46
    モバイルでgit used toを開いている際にtopページ以外のpathに移動できず、
    サインイン/サインアップ/ゲストログインのボタンが消えてモバイルでは使用できない旨の文章が表示されるように処理を追加。

--- frontend/app/src/components/pages/Top.tsx
+++ frontend/app/src/components/pages/Top.tsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom"
 import { guestSignIn } from "lib/api/auth"
 import Cookies from "js-cookie"
 import { AuthContext } from "App"
+import { isMobile } from "react-device-detect"
 
 const useStyles = makeStyles((theme: Theme) => ({
   font: {
@@ -37,6 +38,9 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+  },
+  mobileLimitText: {
+    textAlign: "center"
   }
 }))
 
@@ -93,41 +97,57 @@ const Top: React.FC = () => {
           GIT USED TO
         </motion.div>
       </motion.div>
-      <motion.div
-        className={classes.btn}
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 1.5,
-          delay: 1.2,
-          ease: [0, 0.71, 0.2, 1.01]
-        }}
-      >
+      {!isMobile ? (
         <motion.div
-          className={classes.box}
-          whileHover={{ scale: 1.1 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          onTap={e => {history.push("/signin")}}
-        >
-          <div className={classes.text}>sign in</div>
-        </motion.div>
-        <motion.div
-          className={classes.box}
-          whileHover={{ scale: 1.1 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          onTap={e => {history.push("/signup")}}
-        >
+          className={classes.btn}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1.5,
+            delay: 1.2,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+          >
+          <motion.div
+            className={classes.box}
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            onTap={e => {history.push("/signin")}}
+            >
+            <div className={classes.text}>sign in</div>
+          </motion.div>
+          <motion.div
+            className={classes.box}
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            onTap={e => {history.push("/signup")}}
+            >
           <div className={classes.text}>sign up</div>
+          </motion.div>
+          <motion.div
+            className={classes.box}
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            onTap={handleGuestSubmit}
+            >
+              <div className={classes.text}>ゲストログイン</div>
+          </motion.div>
         </motion.div>
-        <motion.div
-          className={classes.box}
-          whileHover={{ scale: 1.1 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          onTap={handleGuestSubmit}
-        >
-          <div className={classes.text}>ゲストログイン</div>
-        </motion.div>
-      </motion.div>
+        ) : (
+          <motion.div
+            className={classes.mobileLimitText}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 1.5,
+              delay: 1.2,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+            >
+            モバイルではgit used toは使用できません。
+          </motion.div>
+        )
+        }
     </>
   )
 }
