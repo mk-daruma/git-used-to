8ff4471442c1a8187e67d395cbac101b8932949c
mk-daruma <mk69sh@gmail.com>
2023-01-05 00:36:55 +0900

    [update]本番環境のみs3にプロフィール画像を保存する処理を追加 #35
    本番環境のみs3にプロフィール画像を保存する処理を追加。

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,5 +1,24 @@
-CarrierWave.configure  do |config|
-  config.asset_host = "http://localhost:3001"
-  config.storage = :file
-  config.cache_storage = :file
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_public = false
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'git-used-to-web'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/git-used-to-web'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
+    }
+    config.cache_storage = :fog
+  else
+    config.asset_host = "http://localhost:3001"
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
